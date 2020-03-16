b0d2da134eb35f1a32d72f14adffa10291214078
Lucas Teles <lucas.teles@lambda3.com.br>
2020-03-15 22:42:29 -0300

    initial

---
+++ Medieval/Assets/Scripts/CloseOnEsc.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class CloseOnEsc : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Application.Quit();
+
+    }
+}
